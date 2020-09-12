2ab5d68541aee83f7f7201623aa0843084df5c19
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-12 03:59:57 -0400

    App is using speech from mic and consuming api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [speechRecognition, setSpeechRecognition] = useState();
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(()=>{
+    if ('speechSynthesis' in window) {
+      // new speech recognition object
+      const recognitionConstructor =  window.webkitSpeechRecognition;
+      const recognition = new recognitionConstructor();
+      setSpeechRecognition(recognition);
+
+      // Recognition start event handler
+      recognition.onstart = () => { 
+        console.log("it started");
+      }
+
+      recognition.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        console.log(transcript);
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?quorum_factor=1&apikey=80def77c0dbd57d9192eecb88c6b4caf&q_lyrics=${transcript}`).then(res=>{
+        setTracks(res.data.message.body.track_list);
+        }).catch(err=>{
+          console.log(err);
+        })
+      };
+
+    } else {
+        console.log('Speech recognition not supported');
+    }
+  }, [])
+
+  const startSpeechRecognition = () =>{
+    speechRecognition.start();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={startSpeechRecognition}>Click To Search</button>
+      {tracks.map(track=>{
+        track = track.track;
+        return (
+          <div>
+          <p>{track.track_name}</p>
+          <p>{track.artist_name}</p>
+          <a href={track.track_share_url}>See Lyrics</a>
+          </div>
+        )
+      })}
     </div>
   );
 }
