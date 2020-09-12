81964d886f7f29bd0ddb3ad80f8e4d34f9b9a372
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-12 06:32:32 -0400

    Created IsRecording component for display during voice recognition

---
+++ src/components/IsRecording.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IsRecording = () => {
+    return (
+        <div className="is-recording">
+            <span>
+            </span>
+        </div>
+    );
+};
+
+export default IsRecording;
